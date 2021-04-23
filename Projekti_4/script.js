340da63e5b3fd71b8e8faf62988939bd2f0c499b
Leevi12 <leevi.sallantaus@gmail.com>
2021-04-23 12:58:03 +0300

    js/Leevi

--- Projekti_4/script.js
+++ Projekti_4/script.js
@@ -1,10 +1,36 @@
+// Tein nää eri variablet niinkuin ymmärsin
 var lista = document.getElementsByTagName("li");
+var listaJasen = document.createElement("li");
+var teksti = document.getElementById("tekstiKentta").value;
+var t = document.createTextNode(teksti);
+listaJasen.appendChild(document.createTextNode(tekstiKentta.value));
+lista.appendChild(listaJasen);
+tekstiKentta.value = "";
 
 
-function lisaa(){
+//Tää on vaan et se tuo sen tekstikentän pituuden tota lisaa
+//funktiota varten tavallaan
+function kentanPituus()
+{
+       return tekstiKentta.value.length;
+
+// ja taa tuo listan pituuden. siit en vielä tiiä mihin sitä tarvii
+function listanPituus()
+{
+        return lista.length;
+}
+
+
+
+function lisaa() {
+  if (kentanPituus() > 0)
 
 }
 
+
 function poista(){
+
+
+
 
 }
