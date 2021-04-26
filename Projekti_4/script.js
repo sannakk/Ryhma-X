a7f92b66ca75f8df255f5f5ed1c766ef2ebffc22
Leevi12 <leevi.sallantaus@gmail.com>
2021-04-26 11:31:41 +0300

    JS VALMIS/Leevi/26.4.2021

--- Projekti_4/script.js
+++ Projekti_4/script.js
@@ -1,17 +1,19 @@
-// Nappia kun klikkaa
+// Nappia kun klikkaa tapahtuu funktio lisaa1
   var nappi = document.getElementById("enter");
   nappi.addEventListener("click",lisaa1);
 
-//Enteriä kun painaa
+//Enteriä kun painaa tapahtuu funktio lisaa2
   var teksti = document.getElementById("tekstiKentta");
   teksti.addEventListener("keypress", lisaa2);
 
 var ul = document.querySelector("ul");
 
+//Antaa syötetyn tekstin pituuden
 function syotonPituus()
 {
 	return teksti.value.length;
 }
+
 
 function teeLi() {
 // Tekee li elementin
@@ -19,26 +21,24 @@ var li = document.createElement("li");
   // Katsotaan onko syötetty jotain
   if(teksti.value != '')
   {
-    //Lisää tekstin joka on laatikossa
+  //Lisää tekstin joka on laatikossa
     li.appendChild(document.createTextNode(teksti.value));
   	ul.appendChild(li);
   	teksti.value = "";
 
+//Tekee napin jossa x sen edellä luotuun laatikkoon
     var nappi2 = document.createElement("button");
     var x = document.createTextNode("x");
       nappi2.classname = "sulje";
       nappi2.appendChild(x);
       li.appendChild(nappi2);
-      
+      //Tällä funktiolla se poistaa luodun laatikon
+      nappi2.onclick = function() {
+        li.remove(this);
 
-      function poista()
-        {
-      		li.classList.add("delete");
-      	}
+      }
   }
 }
-
-
 
   function lisaa1() {
     //Jos on syötetty jotain suoritetaan TeeLi()
