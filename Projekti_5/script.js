ef84c1a65e123efdad94fc251720553f40da49b6
Sanna Kunnasluoto <sanna.kunnasluoto@hotmail.com>
2021-04-27 21:49:22 +0300

    kommenttien lisäys

--- Projekti_5/script.js
+++ Projekti_5/script.js
@@ -1,10 +1,12 @@
-//Kortin kääntö
-var kortit = document.querySelectorAll('.kortti, .kortti6');
+var kortit = document.querySelectorAll('.kortti, .kortti6'); //Poimii kaikki class "kortti" ja "kortti6" elementit
 var laskuri = 0;
 console.log(kortit);
 
+//Kortin kääntäminen
 function kaanna(){
+  this.classList.toggle('kaanto'); //Tapahtuu kaanto-classiin määritellyt asiat
 console.log('click');
 }
 
-kortit.forEach(kort => kort.addEventListener('click', kaanna));
+//Kutsuu jokaista elementtiä erikseen ja lisää kaikille "tapahtuman" eli klikatessa alkaa funktio kaanna
+kortit.forEach(kortti => kortti.addEventListener('click', kaanna));
