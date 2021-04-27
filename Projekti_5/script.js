ee5825979e6d59c4e82246cf0138a7b0b037c933
Sanna Kunnasluoto <sanna.kunnasluoto@hotmail.com>
2021-04-27 21:24:50 +0300

    js aloitus/Sanna/27.4.2021

--- Projekti_5/script.js
+++ Projekti_5/script.js
@@ -1,2 +1,10 @@
-//PÖÖÖÖÖÖÖÖÖÖÖÖÖÖ
-//Pelästyitkö?
+//Kortin kääntö
+var kortit = document.querySelectorAll('.kortti, .kortti6');
+var laskuri = 0;
+console.log(kortit);
+
+function kaanna(){
+console.log('click');
+}
+
+kortit.forEach(kort => kort.addEventListener('click', kaanna));
