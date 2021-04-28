9ae899d19341eddbf70d74534e80a5d322f2bf17
Sanna Kunnasluoto <sanna.kunnasluoto@hotmail.com>
2021-04-28 13:07:58 +0300

    Lisäyksiä/Sanna

--- Projekti_5/script.js
+++ Projekti_5/script.js
@@ -1,6 +1,7 @@
 var kortit = document.querySelectorAll('.kortti, .kortti6'); //Poimii kaikki class "kortti" ja "kortti6" elementit
 var kaannetty = false;
-var eka, toka;
+var lukitse = false;
+var ekaKortti, tokaKortti;
 //var napautukset = 0;
 //var aikaa = setInterval(ajanLasku, 1000);
 //var sekunnit = 0;
@@ -16,16 +17,39 @@ console.log(sekunnit);
 
 //Kortin kääntäminen
 function kaanna(){
+  if (lukitse) return;
   this.classList.add('kaanto'); //Tapahtuu kaanto-classiin määritellyt asiat
   if (!kaannetty){
     kaannetty = true;
-    eka = this;
-  }else{
-    kaannetty = false;
-    toka = this;
-    console.log({kaannetty, toka});
+    ekaKortti = this;
+    return;
   }
+    tokaKortti = this;
+    kaannetty = false;
+
+onkoParit();
 }
+
+//Onko kortit parit
+function onkoParit(){
+var onkoPari = ekaKortti.dataset.framework === tokaKortti.dataset.framework;
+onkoPari ? poistaKortit(): kaannaTakaisin();
+}
+function poistaKortit(){
+  //On parit
+  ekaKortti.removeEventListener("click", kaanna);
+  tokaKortti.removeEventListener("click", kaanna);
+  //console.log("ei pysty painaa uudestaan");
+}
+
+  //Ei ole parit
+  function kaannaTakaisin(){
+  setTimeout(() => {
+  ekaKortti.classList.remove('kaanto');
+  tokaKortti.classList.remove('kaanto');
+}, 1500);
+  }
+
 
 //Kutsuu jokaista elementtiä erikseen ja lisää kaikille "tapahtuman" eli klikatessa alkaa funktio kaanna
 kortit.forEach(kortti => kortti.addEventListener('click', kaanna));
